28679f62f261f5b5f544e02f48e0808dac42f6de
thitoribeiro <thito.ribeiro@hotmail.com>
2022-01-22 18:35:39 -0300

    Implementing basic abstration with classes

---
+++ cypress/integration/class.spec.js
@@ -0,0 +1,29 @@
+class BasePage {
+    static loadHomePage() {
+        cy.visit('https://devexpress.github.io/testcafe/example')
+    }
+
+    static wait(number) {
+        cy.wait(number)
+    }
+}
+
+class HomePage extends BasePage {
+    static scrollToBottom() {
+        cy.get('#submit-button').scrollIntoView()
+    }
+
+    static scrollToTop() {
+        cy.get('header').scrollIntoView()
+    }
+}
+
+describe('Abstract with classes', () => {
+    it('Should scroll down and up on the page', () => {
+        HomePage.loadHomePage()
+        HomePage.scrollToBottom()
+        HomePage.wait(5000)
+        HomePage.scrollToTop()
+        HomePage.wait(3000)
+    })
+})
