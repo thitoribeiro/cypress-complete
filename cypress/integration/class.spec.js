d6928fe7a43ebca246024f09626a1c5690d0b133
thitoribeiro <thito.ribeiro@hotmail.com>
2022-01-22 18:46:31 -0300

    Including before and after Hooks

--- cypress/integration/class.spec.js
+++ cypress/integration/class.spec.js
@@ -19,8 +19,27 @@ class HomePage extends BasePage {
 }
 
 describe('Abstract with classes', () => {
-    it('Should scroll down and up on the page', () => {
+    before(function() {
+        //runs before all tests inside describe
+        //setup test data or test context
+        //seed or reset the database
         HomePage.loadHomePage()
+    })
+
+    after(function() {
+        //runs after all tests inside describe block are done
+        //test clean up
+        //clean cookies or localStorage
+    })
+
+    beforeEach(function() {
+        //runs before each it block in the describe
+    })
+    afterEach(function() {
+        //runs after each it block in the describe
+    })
+
+    it('Should scroll down and up on the page', () => {
         HomePage.scrollToBottom()
         HomePage.wait(5000)
         HomePage.scrollToTop()
